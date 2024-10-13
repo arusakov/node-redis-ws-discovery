9b71bf00b6209ad717c173180ad1410d52f20eab
Aleksandr Rusakov <aleksandr.s.rusakov@gmail.com>
2024-10-14 01:57:28 +0300

    refactor: lua

--- lua/channelRemove.lua
+++ lua/channelRemove.lua
@@ -1,9 +1,8 @@
 local key = KEYS[1]
 local chnl_key = ARGV[1]
-local chnl_arg = ARGV[2]
 
 local chnl_str = redis.call('HGET', key, chnl_key)
-local chnl, cnt = string.gsub(chnl_str, ','..chnl_arg..',', ',')
+local chnl, cnt = string.gsub(chnl_str, ','..ARGV[2]..',', ',')
 
 if cnt > 0 then
   redis.call('HSET', key, chnl_key, chnl)
